b6a9c8aa332868d8360b86a22d57bad301feb8e1
Wilk Teixeira <wilk-tl@hotmail.com>
2015-08-11 00:27:41 -0300

    Primeiro Commit
    Primeiro Commit do programa Pharma Delivery

---
+++ PharmaDelivery/src/StartPharmDelivery.java
@@ -0,0 +1,17 @@
+
+import View.TelaLogin;
+
+
+
+public class StartPharmDelivery {
+ 
+    
+    public static void main(String[] args){
+    
+        TelaLogin tl = new TelaLogin();
+        tl.setVisible(true);
+    
+    
+    }
+    
+}
